217727ee45dd05eb1562735145569beb59413f52
YINGRONG ZHAO <lionzhao0820@gmail.com>
2017-08-17 13:58:16 -0400

    selection sort

--- Grokking_Algorithm_exercise.js
+++ Grokking_Algorithm_exercise.js
@@ -21,3 +21,58 @@ function binary_search(arr, target){
 }
 
 console.log(binary_search([1, 3, 5, 7, 9], 3))
+
+
+//CHAPTER 2
+
+function findMin(arr){
+  var min = arr[0];
+  var minIndex = 0;
+  for(let i of arr){
+    if(i < min){
+      min = i;
+      minIndex = arr.indexOf(i);
+    }
+  }
+  return minIndex;
+}
+
+function slection_sort(arr){
+  var newArr = [];
+  var arrlength = arr.length;
+  for(let i = 0; i < arrlength; i++){
+    var minIndex = findMin(arr)
+    newArr.push(parseInt(arr.splice(minIndex, 1).toString()));
+  }
+  return newArr;
+}
+
+console.log(slection_sort([5, 3, 6, 2, 10]))
+
+//chapter 3
+
+  //leanier search
+function look_for_key(main_box){
+  var pile = main_box.make_a_pile_to_look_through();
+  while(pile.length > 0){
+    let box = pile.grab_a_box();
+    for(let item of box){
+      if(item.is_a_box()){
+        pile.push(item)
+      }else if(item.is_a_key()){
+        console.log("found the key!");
+      }
+    }
+  }
+}
+
+ //recursion
+function look_for_key_recur(box){
+  for(let item of box){
+    if(item.is_a_box()){
+      look_for_key(item);
+    }else if(item.is_a_key()){
+      console.log("found the key!")
+    }
+  }
+}
